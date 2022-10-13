d58cbcf3bd6b003af4b1e0e7787e0c334ee895dc
k1785 <k1785.penza@gmail.com>
2022-10-13 22:00:10 +0300

    vue topics

--- resources/views/admin/topics/index.blade.php
+++ resources/views/admin/topics/index.blade.php
@@ -8,9 +8,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('topic.add') }}">
-            {{ __('topic.Topics add') }}
-        </a>
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('topic.add') }}">
+                        {{ __('topic.Topics add') }}
+                    </a>
                     <table class="table" style="width: 100%;">
                         @foreach ($topics as $topic)
                             <tr>
@@ -35,6 +35,13 @@
         </div>
     </div>
     {{ $topics->links() }}
+    <div id="app-test-v">
+        <button @click="count++">
+            Count is: @{{ count }}
+        </button>
+    </div>
+    <script src="{{ mix('js/topics.js') }}" defer></script>
+
 </x-app-layout>
 
 
